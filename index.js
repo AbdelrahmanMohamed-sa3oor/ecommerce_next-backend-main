11db47e8f86e2c8757f1e47ae8f00b6323c69c63
AbdelrahmanMohamed-sa3oor <abdelrahmaniphone2@gmail.com>
2025-08-07 12:27:17 +0300

    port

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ const startServer = async () => {
         });
 
         // Start server
-        const PORT =  3000;
+        const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Server started on port ${PORT}`);
             console.log(`Local: http://localhost:${PORT}`);
