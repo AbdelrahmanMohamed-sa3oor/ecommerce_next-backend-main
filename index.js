08659be80269665dc4e6ce1c915da7b87fd3cf9d
AbdelrahmanMohamed-sa3oor <abdelrahmaniphone2@gmail.com>
2025-08-18 19:27:29 +0300

    port handel

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ const startServer = async () => {
         });
 
         // Start server
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT 
         app.listen(PORT, () => {
             console.log(`Server started on port ${PORT}`);
             console.log(`Local: http://localhost:${PORT}`);
